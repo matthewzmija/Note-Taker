fd855d4db714031e0c8b8845732dc03d15918812
Matthew Zmija <matthewzmija@Monicas-MacBook-Air.local>
2023-05-23 16:09:32 -0400

    added comments

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
+// Importing the Express framework and custom router module
 const express = require("express");
 const router = require("./notes");
+
+// Creating an instance of the Express application
 const app = express();
 
+//Middleware
 app.use("/notes", router);
 
+//Exporting the Express application to make it accessible to other modules
 module.exports = app;
--- server.js
+++ server.js
@@ -1,22 +1,31 @@
+//Importing Express framework, path module, and API router
 const express = require("express");
 const path = require("path");
 const api = require("./routes/index");
+
+//Indicating the port number
 const PORT = 3001;
+
+// Creating an instance of the Express application
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/api", api);
 
+// GET request from root path
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
+// GET request from the "/notes" path
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+// Start server and logging successful startup message
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
